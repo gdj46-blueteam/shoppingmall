1133f4b315fd818a870bbbf0e19ab43ddc707a6e
kyuryang <84187694+kyuryang@users.noreply.github.com>
2022-05-10 09:38:26 +0900

    [이규량] 드라이버리스너 추가

---
+++ shoppingmall/src/main/java/listener/driverListener.java
@@ -0,0 +1,21 @@
+package listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class driverListener implements ServletContextListener {
+
+    public void contextInitialized(ServletContextEvent sce)  { 
+    	System.out.println("db드라이버 로딩....");
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		
+    }
+    }
+	
+}

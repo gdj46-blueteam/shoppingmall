25962ff338340765c1fc89081f0f3e292221c146
user <user@LAPTOP-3ARI05KE>
2022-05-13 17:42:57 +0900

    [박선호]AnnouncementDao, SeletAnnoOneController.java, annoList.jsp 추가

--- shoppingmall/src/main/java/dao/AnnouncementDao.java
+++ shoppingmall/src/main/java/dao/AnnouncementDao.java
@@ -1,5 +1,50 @@
 package dao;
 
-public class AnnouncementDao {
+import java.sql.*;
+import java.util.*;
 
+import vo.Announcement;
+
+public class AnnouncementDao {
+	public List<Announcement> selectAnnouncementList( ) {
+		List<Announcement> list = new ArrayList<Announcement>();
+		Announcement announcement;
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		String sql = "SELECT announcement_no announcemetNo, announcement_title announcementTitle, announcement_content announcementContent, create_date createDate"
+				+ " FROM announcement";
+			
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/shoppingmall","root","java1234");
+			stmt = conn.prepareStatement(sql);
+			/*
+			stmt.setInt(1, announcement.getAnnouncementNo());
+			stmt.setString(2, announcement.getAnnouncementTitle());
+			stmt.setString(3, announcement.getAnnouncementContent());
+			stmt.setString(4, announcement.getCreateDate());
+			*/
+			rs = stmt.executeQuery();
+			while(rs.next()) {
+				announcement = new Announcement();
+				announcement.setAnnouncementNo(rs.getInt("announcemetNo"));
+				announcement.setAnnouncementTitle(rs.getString("announcementTitle"));
+				announcement.setAnnouncementContent(rs.getString("announcementContent"));
+				announcement.setCreateDate(rs.getString("CreateDate"));
+				list.add(announcement);
+				System.out.println(announcement.getAnnouncementNo());
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();	
+		}
+		return list;
+	}
 }
+
+	
+
+		
+
+
--- shoppingmall/src/main/webapp/WEB-INF/view/public/annoList.jsp
+++ shoppingmall/src/main/webapp/WEB-INF/view/public/annoList.jsp
@@ -1,5 +1,11 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@page import="vo.*"%>
+<%@ page import="java.sql.*" %>
+<%@ page import="java.util.*" %>
+<%
+	List<Announcement> list = (List<Announcement>)request.getAttribute("announcement");
+%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +13,29 @@
 <title>Insert title here</title>
 </head>
 <body>
-
+	<h1>공지사항 리스트</h1>
+		<form action = "<%=request.getContextPath()%>/SelectAnnoListController" method="post">
+			<table border="1">
+					<tr>
+						<th>No</th>
+						<th>Title</th>
+						<th>Content</th>
+						<th>CreateDate</th>
+					</tr>
+				<%
+					for(Announcement a : list){
+				%>
+					<tr>
+						<td><%=a.getAnnouncementNo()%></td>
+						<td><%=a.getAnnouncementTitle()%></td>
+						<td><%=a.getAnnouncementContent()%></td>
+						<td><%=a.getCreateDate()%></td>
+					</tr>
+				<%
+					}
+				%>
+		
+		</table>
+		</form>
 </body>
 </html>
